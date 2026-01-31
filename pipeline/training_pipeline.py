f4724db335ab9f332d16c7711abb2df079b7bf71
rishabh_Anand <rishabhanand0200@gmail.com>
2026-01-31 12:58:50 +0530

    Training pipeline created

---
+++ pipeline/training_pipeline.py
@@ -0,0 +1,26 @@
+from src.data_ingestion import DataIngestion
+from src.data_preprocessing import DataProcessor
+from src.model_training import ModelTraining
+from utils.common_functions import read_yaml
+from config.paths_config import *
+
+
+if __name__=="__main__":
+    ### 1. Data Ingestion
+
+    data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
+    data_ingestion.run()
+
+    ### 2. Data Processing
+
+    data_processor = DataProcessor(
+        train_path=TRAIN_FILE_PATH,
+        test_path=TEST_FILE_PATH,
+        processed_dir=PROCESSED_DIR,
+        config_path=CONFIG_PATH
+    )
+    data_processor.run()   
+    ### 3. Model Training
+
+    trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH,PROCESSED_TEST_DATA_PATH,MODEL_OUTPUT_PATH)
+    trainer.run() 
